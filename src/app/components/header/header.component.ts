0477adb9fc0dc8ba2f5f324de6b27353774617c0
AlexHorodnic <alexhorodnic@protonmail.com>
2023-03-21 21:49:15 +0200

    remove unused import.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,13 @@
-import {Component, OnChanges, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { ThemeService } from '../../shared/theme.service';
-import {CookieService} from "ngx-cookie-service";
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent{
-  constructor(public themeService: ThemeService, private cookieService: CookieService) {}
+  constructor(public themeService: ThemeService) {}
   theme = 'dark';
 
   switchTheme() {
